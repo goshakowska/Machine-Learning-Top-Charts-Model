b4c64bd70eba4eca288963d7f037acda91cd2aa0
goshakowska <malgorzata.j.kozlowska@gmail.com>
2024-04-11 19:27:07 +0200

    Created project structure in accordance to cookie cutter standard

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A machine learning model for creating in advance top charts for a streaming platform based on the collected data about tracks, users, their sessions etc.',
+    author='Małgorzata Kozłowska',
+    license='MIT',
+)
